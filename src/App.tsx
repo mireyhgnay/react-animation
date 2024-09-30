43bfba5dafa93c06a216bb36a40b4189ca0a0f26
hyerim <hyerimiya1216@gmail.com>
2024-09-30 13:39:46 +0900

    Feat: AnimatePresence 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,69 @@
+import { AnimatePresence, motion } from 'framer-motion';
+import { useState } from 'react';
 import styled from 'styled-components';
-import AnimateBox from './components/AnimateBox';
-import VariantsBox from './components/VariantsBox';
-import GesturesBox from './components/GesturesBox';
-import MotionValue from './components/MotionValue';
-import SvgBox from './components/SvgBox';
+// import AnimateBox from './components/AnimateBox';
+// import VariantsBox from './components/VariantsBox';
+// import GesturesBox from './components/GesturesBox';
+// import MotionValue from './components/MotionValue';
+// import SvgBox from './components/SvgBox';
 
 const Wrapper = styled.div`
   height: 100vh;
   width: 100vw;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+`;
+
+const Grid = styled.div`
   display: grid;
-  place-items: center;
   grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
+`;
+
+const Box = styled(motion.div)`
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  height: 200px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default function App() {
+  const [id, setId] = useState<null | string>(null);
+
   return (
     <Wrapper>
-      <AnimateBox />
-      <VariantsBox />
-      <GesturesBox />
-      <SvgBox />
-      <MotionValue />
+      <Grid>
+        {['1', '2', '3', '4'].map((n) => (
+          <Box onClick={() => setId(n)} key={n} layoutId={n} />
+        ))}
+      </Grid>
+      <AnimatePresence>
+        {id ? (
+          <Overlay
+            initial={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+            animate={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+            exit={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}
+            onClick={() => setId(null)}
+          >
+            <Box layoutId={id} style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
+      </AnimatePresence>
     </Wrapper>
   );
 }
